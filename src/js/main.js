a007566e12564d5abd5dee456ad3ec4850537eed
ingaKuus <IngaKuus13@gmail.com>
2022-05-12 11:13:56 +0300

    ticker works on window resize

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,32 @@
 Array.from(document.getElementsByClassName('ticker')).forEach(ticker => {
   startTicker(ticker, 5)
+  window.onresize = () => {
+    startTicker(ticker, 5, true);
+  }
 })
 
-function startTicker(ticker, speed) {
+function startTicker(ticker, speed, isOnResize) {
   const tickerLabelBlock = ticker.querySelector('.ticker__labels')
   const tickerLabel = ticker.querySelector('.ticker__label');
   tickerLabelW = tickerLabel.clientWidth + Number(window.getComputedStyle(tickerLabel).getPropertyValue('margin-right').slice(0, -2));
 
-  for (let i = 0; i < window.innerWidth / tickerLabelW; i++) {
-    const newLabel = tickerLabel.cloneNode(true);
-    tickerLabelBlock.appendChild(newLabel);
+  const labelsMaxNum = window.innerWidth / tickerLabelW;
+  const labelsCurNum = tickerLabelBlock.children.length;
+  if (labelsCurNum < labelsMaxNum) {
+    for (let i = 0; i <= labelsMaxNum - labelsCurNum; i++) {
+      const newLabel = tickerLabel.cloneNode(true);
+      tickerLabelBlock.appendChild(newLabel);
+    }
   }
 
-  let i = 0;
-  setInterval(() => {
-    if (i >= tickerLabelW) {
-      i = 0;
-    }
-    tickerLabelBlock.style.left = `-${i}px`;
-    i++;
-  }, 50/speed)
+  if (!isOnResize) {
+    let i = 0;
+    setInterval(() => {
+      if (i >= tickerLabelW) {
+        i = 0;
+      }
+      tickerLabelBlock.style.left = `-${i}px`;
+      i++;
+    }, 50/speed)
+  }
 }
