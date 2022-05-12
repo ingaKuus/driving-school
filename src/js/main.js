e9c49697671326c733ff559d7b8f97cffac2ff2d
ingaKuus <IngaKuus13@gmail.com>
2022-05-12 11:04:40 +0300

    ticker fixed

--- src/js/main.js
+++ src/js/main.js
@@ -3,22 +3,21 @@ Array.from(document.getElementsByClassName('ticker')).forEach(ticker => {
 })
 
 function startTicker(ticker, speed) {
+  const tickerLabelBlock = ticker.querySelector('.ticker__labels')
   const tickerLabel = ticker.querySelector('.ticker__label');
   tickerLabelW = tickerLabel.clientWidth + Number(window.getComputedStyle(tickerLabel).getPropertyValue('margin-right').slice(0, -2));
 
   for (let i = 0; i < window.innerWidth / tickerLabelW; i++) {
     const newLabel = tickerLabel.cloneNode(true);
-    ticker.appendChild(newLabel);
+    tickerLabelBlock.appendChild(newLabel);
   }
 
-  Array.from(ticker.children).forEach(label => {
-    let i = 0;
-    setInterval(() => {
-      if (i >= tickerLabelW) {
-        i = 0;
-      }
-      label.style.left = `-${i}px`;
-      i++;
-    }, 50/speed)
-  })
+  let i = 0;
+  setInterval(() => {
+    if (i >= tickerLabelW) {
+      i = 0;
+    }
+    tickerLabelBlock.style.left = `-${i}px`;
+    i++;
+  }, 50/speed)
 }
